0a6b89aa0c7e4c57d275979980ac8f12361848b9
Khairul Haaziq <rhycoz@yahoo.com>
2023-03-17 07:04:36 +0800

    add mono

--- components/SurahList.vue
+++ components/SurahList.vue
@@ -25,7 +25,7 @@
 			<p class="flex-grow">
 				{{ i.name}}
 			</p>
-			<p class="w-20 flex-none text-right opacity-70 text-[13px]">
+			<p class="w-20 flex-none text-right opacity-70 text-[13px]  font-mono tracking-wide">
 				{{formatTime(i.audio[0].duration)}}
 			</p>
 		</div>
@@ -71,7 +71,7 @@
 				class="grid grid-cols-9 items-center justify-center gap-3"
 			>
 				<p
-					class="col-span-2 text-[13px] opacity-70 self-start"
+					class="col-span-2 text-[13px] opacity-70 self-start font-mono  tracking-wide"
 				>
 					{{
 						currentSeek
@@ -159,7 +159,7 @@
 					</div>
 				</div>
 				<p
-					class="col-span-2 text-right text-[13px] opacity-70 self-start"
+					class="col-span-2 text-right text-[13px] opacity-70 self-start  font-mono  tracking-wide"
 				>
 					{{
 						currentDuration
