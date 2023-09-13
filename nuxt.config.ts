027bd97f88634cc2ddf2ef82cbd546b35163f40f
Khairul Haaziq <rhycoz@yahoo.com>
2023-09-13 21:13:58 +0800

    fix build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
         modules: ["@nuxtjs/tailwindcss", "nuxt-icon", "nuxt-headlessui", '@vueuse/nuxt', '@nuxtjs/web-vitals'],
-        ssr: false,
+        ssr: true,
+        nitro: {
+          prerender: {
+            crawlLinks: true,
+            routes: ['/index.html']
+          }
+        }
 })
